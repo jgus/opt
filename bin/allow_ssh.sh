5084ee47a0f33512f276f9842de4df5d9ea7491d
Joshua Gustafson <josh@loomai.com>
2019-05-27 20:41:24 -0600

    allow_ssh

---
+++ bin/allow_ssh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+USER=${1:-jgus}
+
+chmod 600 ~/.ssh/authorized_keys
+curl https://github.com/${USER}.keys >> ~/.ssh/authorized_keys
+uniq -i ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
