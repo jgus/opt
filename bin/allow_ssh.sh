93c70b4ee1ea16543887e8149a932cf5829d515c
Josh Gustafson <josh@loomai.com>
2019-06-08 17:23:46 -0600

    Fix

--- bin/allow_ssh.sh
+++ bin/allow_ssh.sh
@@ -3,6 +3,8 @@ set -e
 
 USER=${1:-jgus}
 
+mkdir -p ~/.ssh
+touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 curl https://github.com/${USER}.keys >> ~/.ssh/authorized_keys
 uniq -i ~/.ssh/authorized_keys
