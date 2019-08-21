c487fca75cf40561b7da7c6acff8f965da6640bd
Josh Gustafson <josh@loomai.com>
2019-08-21 16:11:29 -0600

    fix

--- install-remote.sh
+++ install-remote.sh
@@ -7,7 +7,7 @@ USER=$1
 echo "Cloning repo..."
 ssh ${USER} "[ -d ~/opt ] || git clone https://github.com/jgus/opt.git ~/opt"
 echo "Installing..."
-ssh ${USER} "~/opt/dotfiles/install.sh"
+ssh ${USER} "~/opt/install.sh"
 echo "Copying .ssh..."
 scp ~/.ssh/id_rsa-* ${USER}:~/.ssh
 echo "Fixing up repo URL..."
