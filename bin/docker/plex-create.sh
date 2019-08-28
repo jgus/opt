5f30bc673e47e28a5d02c901bfccb59047969e72
Josh Gustafson <j@gustafson.me>
2019-08-28 07:51:08 -0600

    Plex

---
+++ bin/docker/plex-create.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+docker run \
+  -d \
+  --name plex \
+  --net host \
+  --gpus all \
+  --device /dev/dri:/dev/dri \
+  -e PUID=$(id -u plex) \
+  -e PGID=$(id -g plex) \
+  -e VERSION=latest \
+  -v plex_config:/config \
+  -v plex_media:/media \
+  -v plex_music:/music \
+  -v plex_published:/published \
+  -v /bulk/plex:/bulk \
+  --tmpfs /transcode \
+  --restart always \
+  linuxserver/plex
