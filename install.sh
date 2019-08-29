36a64bbfe5fa5fe217936da682514434a725d1dc
Josh Gustafson <josh@loomai.com>
2019-08-29 16:58:04 -0600

    zsh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 chmod 600 ~/opt/dotfiles/ssh/config
 
-for x in ssh bash_profile bashrc
+for x in ssh bash_profile bashrc zshrc zshrc-grml
 do
 	[ -L ~/.${x} ] || { rm -rf ~/.${x} ; ln -s ~/opt/dotfiles/${x} ~/.${x} ; }
 done
