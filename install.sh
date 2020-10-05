b25873c682fddc83f39e00211a319c58c1377f36
Josh Gustafson <j@gustafson.me>
2020-10-05 10:17:28 -0600

    fix

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PARENT="$(cd "${BASE}/.." ; pwd)"
 
 chmod 600 ${BASE}/dotfiles/ssh/config
 
-for x in ssh bash_profile bashrc zshrc zshrc-grml xsessionrc
+for x in ssh/config bash_profile bashrc zshrc zshrc-grml xsessionrc
 do
 	[ -L ${PARENT}/.${x} ] || { rm -rf ${PARENT}/.${x} ; ln -s ${BASE}/dotfiles/${x} ${PARENT}/.${x} ; }
 done
