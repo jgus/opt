18b4d3ebc2c8b56e1de723ec337054a29cf3c565
Josh Gustafson <j@gustafson.me>
2020-11-12 08:27:30 -0700

    raw-thumbs

---
+++ bin/raw-thumbs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+for DIR in "$@"
+do
+    echo " Looking in ${DIR}..."
+
+    find "${DIR}" -name \*.pp3 -print0 | 
+    while IFS= read -r -d '' PP3
+    do
+        RAW=${PP3%.pp3}
+        if [[ "${PP3}" -nt "${RAW}" ]]
+        then
+            echo "Updating thumbnail in ${RAW}..."
+            TEMP_FILE=$(mktemp)
+            rawtherapee-cli -S -Y -j50 -o "${TEMP_FILE}.jpg" -c "${RAW}"
+            exiftool -OtherImage= -JpgFromRaw= -overwrite_original "${RAW}"
+            case "$(exiftool -Orientation "${RAW}")" in
+                *90\ CW*|*270\ CCW*)
+                    jpegtran -rotate 270 -outfile "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    rm "${TEMP_FILE}.jpg"
+                    mv "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    ;;
+                *90\ CCW*|*270\ CW*)
+                    jpegtran -rotate 90 -outfile "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    rm "${TEMP_FILE}.jpg"
+                    mv "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    ;;
+                *180*)
+                    jpegtran -rotate 180 -outfile "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    rm "${TEMP_FILE}.jpg"
+                    mv "${TEMP_FILE}-rot.jpg" "${TEMP_FILE}.jpg"
+                    ;;
+                *) true ;;
+            esac
+            exiftool -b "-PreviewImage<=${TEMP_FILE}.jpg" -OtherImage= -JpgFromRaw= -overwrite_original "${RAW}"
+            rm "${TEMP_FILE}.jpg"
+        fi
+    done
+done
