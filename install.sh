fff9a19c5568cbadce97b936e1ecb7407ea147cb
Josh Gustafson <j@gustafson.me>
2022-11-02 20:26:07 -0600

    ssh

--- install.sh
+++ install.sh
@@ -5,13 +5,9 @@ set -e
 BASE="$(cd "$( dirname "${BASH_SOURCE[0]}" )" ; pwd)"
 PARENT="$(cd "${BASE}/.." ; pwd)"
 
-chmod 600 ${BASE}/dotfiles/ssh/config
-
 for x in bash_profile bashrc zshrc zshrc-grml xsessionrc
 do
 	[ -L ${PARENT}/.${x} ] || { rm -rf ${PARENT}/.${x} ; ln -s ${BASE}/dotfiles/${x} ${PARENT}/.${x} ; }
 done
-mkdir -p ${PARENT}/.ssh
-cp -f ${BASE}/dotfiles/ssh/config ${PARENT}/.ssh/config
 
 ${BASE}/bin/allow_ssh.sh jgus
